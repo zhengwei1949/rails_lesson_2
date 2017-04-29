f8c74a564ec11fea526f9e7126f859f1a97792f2
zhengwei1949 <zhengwei1949@qq.com>
2017-04-30 00:47:23 +0800

    a

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "topics#index"
+  get 'about' => 'topics#about'
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+    member do
+      post 'downvote'
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
